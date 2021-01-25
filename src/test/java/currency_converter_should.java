1fc9c5a6b2e374366ce712598da00e7fbe633df0
Yoan Thirion <yoan.thirion@gmail.com>
2021-01-25 13:45:56 +0100

    Remove everything to start from scratch

--- src/test/java/currency_converter_should.java
+++ src/test/java/currency_converter_should.java
@@ -2,23 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class currency_converter_should {
-    private final static String FROM_CURRENCY = "USD";
-    private final static String TO_CURRENCY = "EUR";
-
-    private CurrencyConverter currencyConverter;
-
-    @Mock
-    private ChangeRateService changeRateService;
-
     @BeforeEach
     public void init() {
-        currencyConverter = new CurrencyConverter(changeRateService);
+
     }
 
     @Nested
@@ -31,6 +19,6 @@ public class currency_converter_should {
 
     @Nested
     public class return_converted_value_when {
-        
+
     }
 }
